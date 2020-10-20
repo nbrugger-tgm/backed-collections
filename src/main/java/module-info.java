5a768083fd3c2fafa885f4d4a3ec1b49829c3c51
nbrugger-tgm <nbrugger@student.tgm.ac.at>
2020-10-20 14:45:08 +0200

    Switch to non-modular

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-open module niton.collections.backed {
-	requires java.base;
-	requires guava.testlib;
-	exports com.niton.collections.backed;
-	exports com.niton.memory.direct.stores;
-	exports com.niton.memory.direct.managed;
-}
